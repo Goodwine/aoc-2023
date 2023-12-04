4afc1925cf2b6a0cba32927ac7c972a79e4d0226
Carlos Israel Ortiz García <2022649+Goodwine@users.noreply.github.com>
2023-12-04 00:23:30 -0800

    day 4, used allocators, it kinda sux tho

--- day4/main.zig
+++ day4/main.zig
@@ -4,13 +4,22 @@ const std = @import("std");
 const small_txt = @embedFile("small.txt");
 const input_txt = @embedFile("input.txt");
 
-const In = aoc.LineIterator;
+const In = []const Card;
 const Out = usize;
 const Input = aoc.Input(Out);
 const inputs = [_]Input{
-    .{ .name = "small", .input = small_txt, .wantP1 = 3, .wantP2 = 9 },
+    .{ .name = "small", .input = small_txt, .wantP1 = 13, .wantP2 = 9 },
     .{ .name = "large", .input = input_txt, .wantP1 = 5, .wantP2 = 25 },
 };
+
+const Card = struct {
+    winning: std.AutoHashMap(usize, Nothing),
+    numbers: []const usize,
+};
+
+const Nothing = struct {};
+
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 
 pub fn main() !void {
     std.debug.print("\nDay 4\n==========\n", .{});
@@ -22,24 +31,69 @@ test {
 }
 
 fn preprocess(input: []const u8) In {
-    return aoc.lines(input);
+    _ = arena.reset(.retain_capacity);
+
+    var lineIterator = aoc.lines(input);
+    var list = std.ArrayList(Card).init(arena.allocator());
+    while (lineIterator.next()) |line| {
+        var splitter = aoc.splitAny(line, ":|");
+        _ = splitter.next(); // Skip "Card {d}:"
+
+        const winningList = parseInts(usize, splitter.next() orelse "");
+        const numbers = parseInts(usize, splitter.next() orelse "");
+
+        var winning = std.AutoHashMap(usize, Nothing).init(arena.allocator());
+        for (winningList) |w| winning.put(w, .{}) catch unreachable;
+        list.append(.{ .winning = winning, .numbers = numbers }) catch unreachable;
+    }
+    return list.toOwnedSlice() catch unreachable;
+}
+
+fn parseInts(comptime T: type, input: []const u8) []const usize {
+    var list = std.ArrayList(usize).init(arena.allocator());
+    var numberIterator = aoc.splitAny(input, " ");
+    while (numberIterator.next()) |numberRaw| {
+        list.append(std.fmt.parseInt(T, numberRaw, 10) catch unreachable) catch unreachable;
+    }
+    return list.toOwnedSlice() catch unreachable;
 }
 
 fn reset(input: *In) void {
-    input.reset();
+    _ = input;
 }
 
+const one: usize = 1;
+
 fn p1(input: *In) !Out {
-    return len(input);
+    var sum: usize = 0;
+    for (input.*) |card| {
+        var matches: u6 = 0;
+        for (card.numbers) |n| {
+            if (card.winning.contains(n)) matches += 1;
+        }
+        if (matches == 0) continue;
+        sum += one << (matches - 1);
+    }
+    return sum;
 }
 
 fn p2(input: *In) !Out {
-    const l = len(input);
-    return l * l;
-}
+    var listMaker = std.ArrayList(usize).init(arena.allocator());
+    listMaker.appendNTimes(1, input.len) catch unreachable;
+    var ticketTracker = listMaker.toOwnedSlice() catch unreachable;
 
-fn len(input: *In) Out {
-    var c: usize = 0;
-    while (input.next()) |_| c += 1;
-    return c;
+    for (input.*, 0..) |card, id| {
+        var matches: u6 = 0;
+        for (card.numbers) |n| {
+            if (card.winning.contains(n)) matches += 1;
+        }
+        if (matches == 0) continue;
+        for (0..matches) |prize| {
+            // Supposedly this will never ever go out of bouds.
+            ticketTracker[id + prize + 1] += ticketTracker[id];
+        }
+    }
+    var sum: usize = 0;
+    for (ticketTracker) |ticket| sum += ticket;
+    return sum;
 }
