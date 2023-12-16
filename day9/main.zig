4f5cddb07352b00f93e2656f1c6ee592d1663f21
Carlos <2022649+Goodwine@users.noreply.github.com>
2023-12-16 08:31:22 +0000

    day 9 - simple pascal triangles

--- day9/main.zig
+++ day9/main.zig
@@ -4,12 +4,12 @@ const std = @import("std");
 const small_txt = @embedFile("small.txt");
 const input_txt = @embedFile("input.txt");
 
-const In = aoc.LineIterator;
-const Out = usize;
+const In = []const []const Out;
+const Out = isize;
 const Input = aoc.Input(Out);
 const inputs = [_]Input{
-    .{ .name = "small", .input = small_txt, .wantP1 = 3, .wantP2 = 9 },
-    .{ .name = "large", .input = input_txt, .wantP1 = 5, .wantP2 = 25 },
+    .{ .name = "small", .input = small_txt, .wantP1 = 114, .wantP2 = 2 },
+    .{ .name = "large", .input = input_txt, .wantP1 = 2174807968, .wantP2 = 1208 },
 };
 
 pub fn main() !void {
@@ -21,25 +21,64 @@ test {
     try aoc.tester(In, Out, &inputs, preprocess, p1, reset, p2);
 }
 
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+
 fn preprocess(input: []const u8) In {
-    return aoc.lines(input);
+    var buf = std.ArrayList(Out).init(arena.allocator());
+    defer buf.deinit();
+    var lines = aoc.lines(input);
+    var pascal = std.ArrayList([]const Out).init(arena.allocator());
+    defer pascal.deinit();
+    while (lines.next()) |line| {
+        pascal.append(aoc.parseInts(Out, &buf, line, " ")) catch unreachable;
+    }
+    return pascal.toOwnedSlice() catch unreachable;
 }
 
 fn reset(input: *In) void {
-    input.reset();
+    _ = input;
 }
 
 fn p1(input: *In) !Out {
-    return len(input);
+    var buffer = [_]Out{0} ** 1024;
+    defer arena.allocator().free(&buffer);
+    var sum: Out = 0;
+    for (input.*) |row| {
+        std.mem.copyForwards(Out, &buffer, row);
+        var curr = buffer[0..row.len];
+        while (curr.len > 1) : (curr = curr[0 .. curr.len - 1]) {
+            for (curr[1..], 0..) |v, i| {
+                curr[i] = v - curr[i];
+            }
+        }
+        for (buffer[0..row.len]) |v| {
+            sum += v;
+        }
+    }
+    return sum;
 }
 
 fn p2(input: *In) !Out {
-    const l = len(input);
-    return l * l;
+    var buffer = [_]Out{0} ** 1024;
+    defer arena.allocator().free(&buffer);
+    var sum: Out = 0;
+    for (input.*) |row| {
+        std.mem.copyForwards(Out, &buffer, row);
+        var curr = buffer[0..row.len];
+        while (curr.len > 1) : (curr = curr[1..curr.len]) {
+            for (curr[1..], 0..) |_, i| {
+                const idx = curr.len - 1 - i;
+                const v = curr[idx - 1];
+                curr[idx] = v - curr[idx];
+            }
+        }
+        for (buffer[0..row.len]) |v| {
+            sum += v;
+        }
+    }
+    return sum;
 }
 
 fn len(input: *In) Out {
-    var c: usize = 0;
-    while (input.next()) |_| c += 1;
-    return c;
+    _ = input;
 }
